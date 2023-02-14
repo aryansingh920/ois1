66e340f6bc5d446a3db307e7a465432e91c30d43
sainatharjun <sainatharjun4@gmail.com>
2023-02-15 01:22:15 +0530

    responsive by arjun

---
+++ components/milind/Herosection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./stylehero.css";
+export default function herosection(){
+    return(
+        <div className="hero"> 
+            <div className="herosectiongif" >
+                
+                <h1>OLYMPIC INDOOR SPORTS</h1>
+                <h2>Biggest Performance facility in the BA.</h2>
+        
+            </div>
+        </div>
+    )
+}
